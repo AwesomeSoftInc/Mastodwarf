e42e9b98945217383040dcc2b2789ca8e338ec76
IoIxD <alphaproject217@gmail.com>
2022-11-22 01:03:08 -0700

    first working(?) commit

--- src/main/java/net/ioixd/spigotmc/plugin/commands/MastodwarfClient.java
+++ src/main/java/net/ioixd/spigotmc/plugin/commands/MastodwarfClient.java
@@ -4,9 +4,17 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class MastodwarfClient extends JavaPlugin {
 
+    public void main() {
+        // dummy
+    }
+
     @Override
     public void onEnable() {
         this.getCommand("mastodwarf").setExecutor(new MastodwarfCommand());
         getLogger().info("Added the 'mastodwarf' command.");
+    }
+
+    public void log(String msg) {
+        getLogger().info(msg);
     }
 }
--- src/main/java/net/ioixd/spigotmc/plugin/commands/MastodwarfCommand.java
+++ src/main/java/net/ioixd/spigotmc/plugin/commands/MastodwarfCommand.java
@@ -1,82 +1,120 @@
 package net.ioixd.spigotmc.plugin.commands;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.StringWriter;
-import java.util.List;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
+import com.google.gson.*;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.BookMeta;
 
-import com.comphenix.protocol.PacketType;
-import com.comphenix.protocol.ProtocolLibrary;
-import com.comphenix.protocol.events.PacketContainer;
-import com.google.gson.Gson;
-import com.sys1yagi.mastodon4j.MastodonClient;
-import com.sys1yagi.mastodon4j.api.Pageable;
-import com.sys1yagi.mastodon4j.api.Range;
-import com.sys1yagi.mastodon4j.api.entity.Status;
-import com.sys1yagi.mastodon4j.api.exception.Mastodon4jRequestException;
-import com.sys1yagi.mastodon4j.api.method.Timelines;
-
-import okhttp3.OkHttpClient;
-
-
-
 public class MastodwarfCommand implements CommandExecutor {
+
+    Pattern html = Pattern.compile("<(.*?)>");
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String s, String[] strings) {
         ItemStack book = this.newBook("timeline");
-
         Player p = Bukkit.getServer().getPlayer(sender.getName());
+
         int slot = p.getInventory().getHeldItemSlot();
         ItemStack old = p.getInventory().getItem(slot);
         p.getInventory().setItem(slot, book);
-
-        try {
-             PacketContainer pc = ProtocolLibrary.getProtocolManager().createPacket(PacketType.Play.Server.CUSTOM_PAYLOAD);
-             pc.getModifier().writeDefaults();
-             pc.getStrings().write(0, "MC|BOpen");
-             ProtocolLibrary.getProtocolManager().sendServerPacket(p, pc);
-        } catch (Exception e) {
-             e.printStackTrace();
-             return false;
-        }
-
+        p.openBook(book);
         p.getInventory().setItem(slot, old);
         return true;
+    }
+
+    class Status{
+        long id;
+        String content;
+        Account account;
+
+    }
+
+    class Account {
+        String username;
+        String display_name;
     }
 
     public ItemStack newBook(String arg) {
         ItemStack book = new ItemStack(Material.WRITTEN_BOOK);
         BookMeta meta = (BookMeta) book.getItemMeta();
 
-        MastodonClient client = new MastodonClient.Builder("mstdn.jp", new OkHttpClient.Builder(), new Gson()).build();
-        Timelines timelines = new Timelines(client);
+        meta.setTitle("Mastodwarf");
+        meta.setAuthor("god");
 
         try {
+            URL url = new URL("https://wetdry.world/api/v1/timelines/public?local=true");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            int response = conn.getResponseCode();
+            String data;
+            if(response == 200) {
+                BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                data = br.lines().collect(Collectors.joining());
+            } else {
+                data = conn.getResponseMessage();
+            }
+            Gson gson = new Gson();
+            Status[] statuses = gson.fromJson(data, Status[].class);
+
+            int advance = (ChatColor.BOLD.toString()+ChatColor.RESET.toString()+
+                    ChatColor.ITALIC.toString()+ChatColor.GRAY.toString()+
+                    ChatColor.RESET).length();
+
+            int offset = 0;
             StringWriter writer = new StringWriter();
-            Pageable<Status> statuspages = timelines.getHome(new Range()).execute();
-            List<Status> statuses = statuspages.getPart();
+            for(int i = 0; i < statuses.length; i++) {
+                Status status = statuses[i];
 
-            statuses.forEach(status->{
-                writer.write("=============");
-                writer.write(status.getAccount().getDisplayName());
-                writer.write(status.getContent());
-                if(writer.getBuffer().length() >= 256) {
-                    meta.addPage(writer.getBuffer().toString());
-                    writer.flush();
+                if(status.content.compareTo("") != 0) {
+                    writer.write(ChatColor.BOLD+status.account.display_name+" "+ChatColor.RESET
+                            +ChatColor.ITALIC+ChatColor.GRAY+"(@"+status.account.username+"):"+ChatColor.RESET
+                            +"\n");
+                    offset += advance;
+
+                    String content = (status.content).replaceAll("</p>","\n");
+                    content = (content).replaceAll("<br>","\n");
+                    content = html.matcher(content).replaceAll("");
+
+
+                    writer.write(content+"\n\n");
                 }
-            });
-          } catch (Exception e) {
-            e.printStackTrace();
-          }
 
+                StringBuffer buf = writer.getBuffer();
+                int len = buf.length();
+                final int MAX = 150;
+                if(len > MAX) {
+                    meta.addPage(buf.toString());
+                    String remaining = buf.substring(MAX-offset, len);
+                    buf.setLength(0);
+                    writer.write(remaining);
+                    offset = 0;
+                }
+            }
+            writer.close();
+        } catch (Exception e) {
+            String msg = e.getMessage();
+            if(e.getMessage().length() != 0) {
+                meta.addPage("Error: "+msg);
+            } else {
+                meta.addPage("Error: fucking none the code gave us None. I Love Java.");
+            }
+            e.printStackTrace();
+        }
+        book.setItemMeta(meta);
         return book;
     }
 }
